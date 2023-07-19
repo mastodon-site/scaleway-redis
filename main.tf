6b1d655445e208fb2d7eb01f12a1ad5f5f37842c
grrywlsn <6753180+grrywlsn@users.noreply.github.com>
2023-07-20 00:17:29 +0100

    Update to s3 remote state

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 terraform {
-  backend "gcs" {}
+  backend "s3" {}
   required_version = "> 1.0.0"
 }
 
 provider "scaleway" {
-  zone       = var.scaleway_zone
-  region     = var.scaleway_region
-  project_id = var.scaleway_project_id
+  zone            = var.scaleway_zone
+  region          = var.scaleway_region
+  project_id      = var.scaleway_project_id
+  organization_id = var.scaleway_organization_id
 }
